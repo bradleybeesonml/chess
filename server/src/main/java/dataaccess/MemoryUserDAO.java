40fd821bbbcb6430699065e3bf1ac8aca719fd48
bradleybeesonml <bbeeson1@byu.edu>
2025-10-14 10:15:34 -0600

    Add MemoryDAO classes

--- server/src/main/java/dataaccess/MemoryUserDAO.java
+++ server/src/main/java/dataaccess/MemoryUserDAO.java
@@ -1,5 +1,23 @@
 package dataaccess;
 import model.UserData;
-public class MemoryUserDAO {
-    
+import java.util.Map;
+import java.util.HashMap;
+
+public class MemoryUserDAO implements UserDAO {
+    private final Map<String, UserData> users = new HashMap<>();
+
+    public void createUser(UserData user) throws DataAccessException {
+        if (users.containsKey(user.username())) {
+            throw new DataAccessException("User already exists");
+        }
+        users.put(user.username(), user);
+    }
+
+    public UserData getUser(String username){
+        return users.get(username);
+    }
+
+    public void clear() {
+        users.clear();
+    }
 }
--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -14,8 +14,21 @@ public class UserService {
     }
 
     public RegisterResult register(RegisterRequest request) throws DataAccessException {
+        if (request.username() == null || request.password() == null || request.email() == null ||
+                request.username().isEmpty() || request.password().isEmpty() || request.email().isEmpty()) {
+            throw new DataAccessException("Error: bad request");
+        }
 
-    
+        if (userDAO.getUser(request.username()) != null) {
+            throw new DataAccessException("Already taken");
+        }
 
-    
+        UserData newUser = new UserData(request.username(), request.email(), request.password());
+        userDAO.insertUser(newUser);
+
+        String authToken = UUID.randomUUID().toString();
+        authDAO.insertAuth(new AuthData(authToken, request.username()));
+
+        return new RegisterResult(request.username(), authToken);
+    }
 }
