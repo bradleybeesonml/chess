431c4e7b573501a33bf66429a0a906b328ffb7ff
bradleybeesonml <bbeeson1@byu.edu>
2025-10-13 18:29:06 -0600

    Add UserDAO and GameDAO

---
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -0,0 +1,11 @@
+package dataaccess;
+import model.GameData;
+import java.util.Collection;
+
+public interface GameDAO {
+    GameData insertGame(String gameName) throws DataAccessException;
+    GameData getGame(int gameID) throws DataAccessException;
+    Collection<GameData> listGames() throws DataAccessException;
+    void updateGame(int gameID, GameData game) throws DataAccessException;
+    void clear() throws DataAccessException;
+}
