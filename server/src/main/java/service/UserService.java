ec9e67c4e794b40682c3a069935f240ba33bbc9b
bradleybeesonml <bbeeson1@byu.edu>
2025-10-14 09:59:45 -0600

    Add user data access object in memory

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -6,5 +6,16 @@ import java.util.UUID;
 public class UserService {
     private final UserDAO userDAO;
     private final AuthDAO authDAO;
+
+    public UserService(UserDAO userDAO, AuthDAO authDAO) {
+        this.authDAO = authDAO;
+        this.userDAO = userDAO;
+
+    }
+
+    public RegisterResult register(RegisterRequest request) throws DataAccessException {
+
+    
+
     
 }
