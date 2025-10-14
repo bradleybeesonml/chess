e945684973a9efb0bf11dae0af648c60c5677348
bradleybeesonml <bbeeson1@byu.edu>
2025-10-14 09:49:50 -0600

    Add services package

---
+++ server/src/main/java/service/UserService.java
@@ -0,0 +1,10 @@
+package service;
+import model.*;
+import dataaccess.*;
+import java.util.UUID;
+
+public class UserService {
+    private final UserDAO userDAO;
+    private final AuthDAO authDAO;
+    
+}
