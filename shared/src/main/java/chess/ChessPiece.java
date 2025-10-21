368de6712cac93f1471c246fa070380aa735d9b2
bradleybeesonml <bbeeson1@byu.edu>
2025-10-21 11:19:26 -0600

    Add helper function for 'continuosly' moving pieces. Refactor rookMoves. (Code quality fix)

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -76,45 +76,44 @@ public class ChessPiece {
         }
     }
 
+    private Collection<ChessMove> continuousMoves(ChessBoard board, ChessPosition myPosition, int[][] directions) {
+        List<ChessMove> moves = new ArrayList<>();
+
+        for (int[] direction : directions) {
+            int rowStep = direction[0];
+            int colStep = direction[1];
+
+            int currentRow = myPosition.getRow() + rowStep;
+            int currentCol = myPosition.getColumn() + colStep;
+
+            while (currentRow >= 1 && currentRow <= 8 && currentCol >= 1 && currentCol <= 8) {
+                ChessPosition newPosition = new ChessPosition(currentRow, currentCol);
+                ChessPiece pieceAtNewPosition = board.getPiece(newPosition);
+
+                if (pieceAtNewPosition == null) {
+                    moves.add(new ChessMove(myPosition, newPosition, null));
+                } else if (pieceAtNewPosition.getTeamColor() != this.getTeamColor()) {
+                    moves.add(new ChessMove(myPosition, newPosition, null));
+                    break;
+                } else {
+                    break;
+                }
+
+                currentRow += rowStep;
+                currentCol += colStep;
+            }
+        }
+        return moves;
+    }
+
     private Collection<ChessMove> rookMoves(ChessBoard board, ChessPosition myPosition) {
-        List<ChessMove> moves = new ArrayList<>(); //create a List of possible moves
-        int[][] possibledirections = { //rooks can move up, down, left, or right one square at a time until blocked
+        int[][] rookDirections = {
                 {0, 1},
                 {1, 0},
                 {-1, 0},
                 {0, -1}
         };
-        for (int[] direction : possibledirections) {
-            int rowStep = direction[0]; //split the current possible direction into x and y steps
-            int colStep = direction[1];
-
-            int currentRow = myPosition.getRow() + rowStep; //update the row
-            int currentCol = myPosition.getColumn() + colStep; //update the column
-
-            while (currentRow >= 1 && currentRow <= 8 && currentCol >= 1 && currentCol <= 8) { //make sure new row and column are within the bounds of the board
-                ChessPosition newPosition = new ChessPosition(currentRow, currentCol); //create a new ChessPosition instance
-                ChessPiece pieceAtNewPosition = board.getPiece(newPosition); //Get the piece at new position (if there is no piece, will return null)
-
-                if (pieceAtNewPosition == null) { //if null, the step is valid and this is a possible move
-                    moves.add(new ChessMove(myPosition, newPosition, null));
-                }
-
-                else if (pieceAtNewPosition.getTeamColor() != this.getTeamColor()) { //if there is a piece of the opposite color, it can be taken but the movement must stop
-                    moves.add(new ChessMove(myPosition, newPosition, null));
-                    break; //break the movement loop because we captured a piece
-                }
-                else {
-                    break; //break because we must have encountered a piece of our own color (or the edge of the board), so the last valid move is the last one we added
-                }
-
-
-                currentRow += rowStep; //continue to to the next step if we haven't broken the loop yet
-                currentCol += colStep;
-            }
-
-        }
-        return moves;
-
+        return continuousMoves(board, myPosition, rookDirections);
     }
 
     private Collection<ChessMove> queenMoves(ChessBoard board, ChessPosition myPosition) {
