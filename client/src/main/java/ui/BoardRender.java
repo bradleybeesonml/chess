b499d07c263f271b5fdb1ee14e8c0b8c02493e56
bradleybeesonml <bbeeson1@byu.edu>
2025-11-13 00:08:40 -0700

    draw header rows for white and black (with a clean white bg)

--- client/src/main/java/ui/BoardRender.java
+++ client/src/main/java/ui/BoardRender.java
@@ -7,20 +7,43 @@ public class BoardRender {
     
     public static void drawWhiteBoard(ChessBoard board) {
         System.out.print(ERASE_SCREEN);
-        System.out.println("Drawing board for white player");
+        drawHeaderRow(true);
+    }
+
+    public static void drawBlackBoard(ChessBoard board) {
+        System.out.print(ERASE_SCREEN);
+        drawHeaderRow(false);
     }
 
 
     public static void main(String[] args) {
         ChessBoard board = new ChessBoard();
         board.resetBoard();
-        System.out.println("Testing from White Players' perspective");
+        System.out.println("White:");
         drawWhiteBoard(board);
+
+        System.out.print(RESET_BG_COLOR);
+        System.out.print(RESET_TEXT_COLOR);
+        System.out.println("\nBlack:");
+        drawBlackBoard(board);
 
     }
 
     private static void drawHeaderRow(boolean isWhite){
-        
+        System.out.print(EscapeSequences.SET_TEXT_COLOR_DARK_GREY);
+        System.out.print(EscapeSequences.SET_BG_COLOR_WHITE);
+
+        if(isWhite){
+            for (char columnLetter = 'a'; columnLetter <= 'h'; columnLetter++) {
+                System.out.print(" " + columnLetter + " ");
+            }
+        }
+
+        else {
+            for (char columnLetter = 'h'; columnLetter >= 'a'; columnLetter--) {
+                System.out.print(" " + columnLetter + " ");
+            }
+        }
     }
 
 
