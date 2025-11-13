d8c637453a387c4c84032ca5bd1c09a0aac6487c
bradleybeesonml <bbeeson1@byu.edu>
2025-11-13 00:20:18 -0700

    Drawing first row for white correctly with row number and header:

--- client/src/main/java/ui/BoardRender.java
+++ client/src/main/java/ui/BoardRender.java
@@ -8,6 +8,7 @@ public class BoardRender {
     public static void drawWhiteBoard(ChessBoard board) {
         System.out.print(ERASE_SCREEN);
         drawHeaderRow(true);
+        drawRow(board, 1, true);
     }
 
     public static void drawBlackBoard(ChessBoard board) {
@@ -34,9 +35,11 @@ public class BoardRender {
         System.out.print(EscapeSequences.SET_BG_COLOR_WHITE);
 
         if(isWhite){
+            System.out.print("   ");
             for (char columnLetter = 'a'; columnLetter <= 'h'; columnLetter++) {
                 System.out.print(" " + columnLetter + " ");
             }
+            System.out.print("   ");
         }
 
         else {
@@ -44,6 +47,65 @@ public class BoardRender {
                 System.out.print(" " + columnLetter + " ");
             }
         }
+    }
+
+    private static String getPieceSymbol(chess.ChessPiece piece) {
+        if (piece.getTeamColor() == chess.ChessGame.TeamColor.WHITE) {
+            return switch (piece.getPieceType()) {
+                case KING -> EscapeSequences.WHITE_KING;
+                case QUEEN -> EscapeSequences.WHITE_QUEEN;
+                case BISHOP -> EscapeSequences.WHITE_BISHOP;
+                case KNIGHT -> EscapeSequences.WHITE_KNIGHT;
+                case ROOK -> EscapeSequences.WHITE_ROOK;
+                case PAWN -> EscapeSequences.WHITE_PAWN;
+            };
+        } else {
+            return switch (piece.getPieceType()) {
+                case KING -> EscapeSequences.BLACK_KING;
+                case QUEEN -> EscapeSequences.BLACK_QUEEN;
+                case BISHOP -> EscapeSequences.BLACK_BISHOP;
+                case KNIGHT -> EscapeSequences.BLACK_KNIGHT;
+                case ROOK -> EscapeSequences.BLACK_ROOK;
+                case PAWN -> EscapeSequences.BLACK_PAWN;
+            };
+        }
+    }
+
+    private static void drawRow(ChessBoard board, int row, boolean isWhite) {
+        System.out.print(EscapeSequences.RESET_BG_COLOR);
+        System.out.println("");
+        System.out.print(EscapeSequences.SET_BG_COLOR_WHITE);
+        System.out.print(EscapeSequences.SET_TEXT_COLOR_BLACK);
+        System.out.print(" " + row + " ");
+
+        for (int col = 1; col <= 8; col++) {
+            int actualCol = isWhite ? col : (9 - col);
+
+            boolean isLightSquare = (row + actualCol) % 2 == 1;
+
+            if (isLightSquare) {
+                System.out.print(EscapeSequences.SET_BG_COLOR_WHITE);
+            } else {
+                System.out.print(EscapeSequences.SET_BG_COLOR_DARK_GREEN);
+            }
+
+            chess.ChessPosition position = new chess.ChessPosition(row, actualCol);
+            chess.ChessPiece piece = board.getPiece(position);
+
+            if (piece == null) {
+                System.out.print(EscapeSequences.EMPTY);
+            } else {
+                System.out.print(getPieceSymbol(piece));
+            }
+        }
+
+        System.out.print(EscapeSequences.SET_BG_COLOR_WHITE);
+        System.out.print(EscapeSequences.SET_TEXT_COLOR_BLACK);
+        System.out.print(" " + row + " ");
+
+        System.out.print(EscapeSequences.RESET_BG_COLOR);
+        System.out.print(EscapeSequences.RESET_TEXT_COLOR);
+        System.out.println();
     }
 
 
